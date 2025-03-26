39a196de8588f1ff959fd1176b86ef018ac94e86
github_cloudlabsuser_2146@cloudlabsaiuser.com <github_cloudlabsuser_2146@cloudlabsaiuser.com>
2025-03-26 17:46:02 +0000

    Enhance User model validation and add search functionality to User index view

--- Models/User.cs
+++ Models/User.cs
@@ -1,16 +1,21 @@
-
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace MyMvcApp.Models
 {
     public class User
     {
         public int Id { get; set; }
-        public string Name { get; set; }
-        public string Email { get; set; }
 
+        [Required(ErrorMessage = "Name is required.")]
+        [StringLength(100, ErrorMessage = "Name cannot exceed 100 characters.")]
+        public string Name { get; set; } = string.Empty;
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid email format.")]
+        public string Email { get; set; } = string.Empty;
     }
 }
