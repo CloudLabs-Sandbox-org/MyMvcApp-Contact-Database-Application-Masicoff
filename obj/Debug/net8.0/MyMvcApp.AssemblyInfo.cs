c89c4e56db2b71e5e7093a16c377d45b70d41ef2
github_cloudlabsuser_2146@cloudlabsaiuser.com <github_cloudlabsuser_2146@cloudlabsaiuser.com>
2025-03-26 19:16:55 +0000

    Update assembly informational version and cache inputs

--- obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a6fff28f6c0feff02ca9e0d2f4602867f8f1d3d1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d453934b9bae72519977825719113bd3c4b063e4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
